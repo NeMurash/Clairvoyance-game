dee1803d09329664f15b0af280f84f60da2f9f09
NeMurash <dimabelida5@gmail.com>
2026-01-17 18:20:08 +0100

    myUtils.h cleanup

--- myUtils.h
+++ myUtils.h
@@ -2,19 +2,17 @@
 #include <SDL3/SDL.h>
 #define MY_UTILS_H
 
+// DECLARATION
+
 // Map a number that falls between min0 and max0 to a number that falls between min1 and max1
 float mapNumber(float n, float min0, float max0, float min1, float max1);
-
 // Screen To World Coordinates Mapping ((-1, 1), (-1, 1)) -> ((0, minmax.x), (0, minmax.y))
 SDL_FPoint STWCoords(SDL_FPoint point, SDL_FPoint minmax);
-
 // Linear interpolation of a vector (https://en.wikipedia.org/wiki/Linear_interpolation)
 SDL_FPoint lerpV(SDL_FPoint v0, SDL_FPoint v1, float t);
-
 // Check if a point is inside of a rectangular region
 bool pointInRect(SDL_FPoint pt, SDL_FRect rect);
 
-// A tool that will help us later
 struct Card {
 	SDL_Texture* texture;
 	SDL_FPoint   position;
@@ -29,6 +27,8 @@ enum CardType {
 	CARD_SQUARE,
 	CARD_STAR
 };
+
+// IMPLEMENTATION
 
 float mapNumber(float n, float min0, float max0, float min1, float max1) {
 	return (n - min0) / (max0 - min0) * (max1 - min1) + min1;
